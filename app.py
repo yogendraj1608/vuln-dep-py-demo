a2ce7c06bbea13157019d4cfa93e81d599b1a45c
yogendraj1608 <jainyogendra1685@gmail.com>
2025-08-04 17:24:18 +0530

    Patched app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-import requests\nprint(requests.__version__)
+from flask import Flask
+import requests
+import yaml
+import urllib3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Dependency patched app running securely."
+
+if __name__ == "__main__":
+    app.run(debug=True)
